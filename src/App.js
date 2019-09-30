7fa33368cb1a3cbee74b4de3be53e64c80be2a13
devyboy <dev@udel.edu>
2019-09-30 17:11:40 -0400

    componentization baby

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import firebase from "firebase";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from './pages/HomePage';
@@ -8,33 +8,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Firebase Credentials
 var config = {
-  apiKey: "AIzaSyA-PcLom0MmPjP-rc9WL2dxs8HsD9QFdag",
-  authDomain: "ayylmao-5b8f8.firebaseapp.com",
-  databaseURL: "https://ayylmao-5b8f8.firebaseio.com",
-  projectId: "ayylmao-5b8f8",
-  storageBucket: "ayylmao-5b8f8.appspot.com",
-  messagingSenderId: "1060743996645",
-  appId: "1:1060743996645:web:7d96b40912d25ffb84e00c"
+    apiKey: "AIzaSyA-PcLom0MmPjP-rc9WL2dxs8HsD9QFdag",
+    authDomain: "ayylmao-5b8f8.firebaseapp.com",
+    databaseURL: "https://ayylmao-5b8f8.firebaseio.com",
+    projectId: "ayylmao-5b8f8",
+    storageBucket: "ayylmao-5b8f8.appspot.com",
+    messagingSenderId: "1060743996645",
+    appId: "1:1060743996645:web:7d96b40912d25ffb84e00c"
 };
 
 firebase.initializeApp(config);
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return(
-            <Router>
-                <Switch>
-                        <Route exact path="/" render={(props) => <HomePage {...props} />} />
-                        <Route component={FourOhFour} />
-                </Switch>
-            </Router>
-        );
-    }
+function App(props) {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" render={(props) => <HomePage {...props} />} />
+                <Route render={(props) => <FourOhFour {...props} />} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/FourOhFour.js
+++ src/pages/FourOhFour.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import Menu from '../components/menu';
 
 
-class Thread extends Component {
-    render() {
-        return(
-            <div className="App">
-                <h1>{this.props.location.pathname} not found.</h1>
-            </div>
-        );
-    }
+function FourOhFour(props) {
+    return (
+        <div className="App">
+            <Menu />
+            <h1>{props.location.pathname} not found</h1>
+        </div>
+    );
 }
 
-export default Thread;
+export default FourOhFour;
