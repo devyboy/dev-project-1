3f360c6dc1f69a2bfdea4af2c04d79ca2b71d922
devyboy <dev@udel.edu>
2019-11-05 23:07:25 -0500

    download exam in formats n stuff

--- src/pages/GeneratePage.js
+++ src/pages/GeneratePage.js
@@ -1,91 +1,114 @@
 import React from 'react';
-import firebase from 'firebase';
-import { PDFExport } from '@progress/kendo-react-pdf';
 import Menu from '../components/menu';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import Button from '@material-ui/core/Button';
+import YAML from 'yaml';
+
 
 class Generate extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    
+      questions: null,
+      format: ".json",
+      filename: "",
     }
-    this.downloadPDF = this.downloadPDF.bind(this);
+
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.downloadFile = this.downloadFile.bind(this);
   }
 
-  fetchQuestions() {
-    let questionArray= [];
-    let questionsRef = firebase.firestore().collection('questions');
-    questionsRef.get().then(snapshot => {
-      snapshot.forEach(doc => {
-        questionArray.push(doc.data());
-        this.setState({ questions: questionArray });
-      });
-    }).catch(err => {
-        console.log(err);
-    })
+  componentDidMount() { 
+    if (this.props.location.state === undefined) {
+      window.location.href = "/view-edit"
+    }
+    else {
+      this.setState({ questions: this.props.location.state.questions });
+    }
   }
 
-  componentDidMount() {
-    this.fetchQuestions();
+  handleFormatChange(event) {
+    this.setState({ format: event.target.value });
   }
 
-  downloadPDF() {
-    this.resume.save();
+  downloadFile() {
+    var fileDownload = require('js-file-download');
+    console.log(this.state.format);
+    switch(this.state.format) {
+      case ".json":
+        fileDownload(JSON.stringify(this.state.questions), this.state.filename + this.state.format);
+        break;
+      case ".yaml":
+        fileDownload(YAML.stringify(this.state.questions), this.state.filename + this.state.format);
+        break;
+      case ".txt":
+        fileDownload(this.convertToText(this.state.questions), this.state.filename + this.state.format);
+        break;
+      default:
+        fileDownload(this.convertToText(this.state.questions), this.state.filename + this.state.format);
+        break;
+    }
+  }
+
+  convertToText(questions) {
+    console.log(questions);
+    let str = ""
+    let i = 1;
+    questions.forEach((q) => {
+      str += (i + ". " + q.question);
+      if (q.choices.length > 0) {
+        str += ("\n   A. " + q.choices[0]);
+        str += ("\n   B. " + q.choices[1]);
+        str += ("\n   C. " + q.choices[2]);
+        str += ("\n   D. " + q.choices[3] + "\n\n");
+      }
+      else {
+        str += "\n\n";
+      }
+
+      i++;
+    });
+    return str;
+  }
+
+  handleNameChange(event) {
+    this.setState({ filename: event.target.value });
   }
 
   render() {
     return(
       <div className="App">
         <Menu />
-        <h2>Generate Exam</h2>
-        <br />
-        <PDFExport paperSize={'Letter'}
-          fileName="Exam.pdf"
-          title=""
-          subject=""
-          keywords=""
-          ref={(r) => this.resume = r}>
-              <div style={{
-                  height: 792,
-                  width: 612,
-                  padding: '20px',
-                  backgroundColor: 'white',
-                  boxShadow: '5px 5px 5px 5px grey',
-                  margin: 'auto',
 
-                  overflowX: 'hidden',
-                  overflowY: 'hidden'}}>
-                      {this.state.questions ? this.state.questions.map((q, key) => (
-                        <div key={key}>
-                          <strong>Unit: </strong>{q.unit},&nbsp;
-                          <strong>Topic: </strong>{q.topic},&nbsp;
-                          <strong>Course: </strong>CISC{q.course},&nbsp;
-                          <strong>Difficulty: </strong>{q.diff},&nbsp;
-                          <strong>Type: </strong>{q.type},&nbsp;
-                          <strong>Answer: </strong>{q.answer}&nbsp;
-                          <br />
-                          {q.choices.length !== 0 ? 
-                          <div>
-                            <strong>Choices: </strong>{q.choices.map((choice) => {
-                            return(
-                                choice + "; "
-                              );
-                          })}
-                          </div>
-                          :
-                          null
-                          }
-                        </div>
-                      ))
-                      :
-                      null
-                      }
-              </div>
-          </PDFExport>
-        <Button variant="contained" color="primary" onClick={this.downloadPDF} style={{ margin: '2em'}}>
-          Download
-        </Button>
+        {this.state.questions ? 
+          <div>
+            <h2>Generate Exam</h2>
+
+            <Form.Group as={Col} md="2">
+              <Form.Label>File Name</Form.Label>
+              <Form.Control onChange={this.handleNameChange} value={this.state.filename}>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} md="1">
+              <Form.Label>File Format</Form.Label>
+              <Form.Control onChange={this.handleFormatChange} as="select" value={this.state.format}>
+                <option>.json</option>
+                <option>.yaml</option>
+                <option>.txt</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Button color="primary" variant="contained" onClick={this.downloadFile}>
+              Download
+            </Button>
+          </div>
+          :
+          null
+        }
+
       </div>
     );
   }
