a34127e7f80a8b087f28cba2ba9cf3379fcbdd2c
devyboy <dev@udel.edu>
2019-11-17 19:01:28 -0500

    updated firebase imports

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 import { Link } from 'react-router-dom';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
