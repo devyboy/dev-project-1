0e12b6e1a80510e80c894b6c748358458c96ce11
devyboy <dev@udel.edu>
2019-11-25 15:57:23 -0500

    moved print to modal, html download valid page

--- src/pages/ExamPage.js
+++ src/pages/ExamPage.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import Fab from "@material-ui/core/Fab";
 import DownloadIcon from "@material-ui/icons/GetApp";
-import PrintIcon from "@material-ui/icons/Print";
 import YAML from 'yaml';
+import "../exam.css";
 import {
     DialogContent,
     Dialog,
@@ -61,7 +61,18 @@ class ExamPage extends React.Component {
         switch (this.state.format) {
             case ".html":
                 let pageHTML = document.getElementById("exam").innerHTML;
-                fileDownload(pageHTML, this.state.filename + this.state.format);
+                let template =
+                    `<!DOCTYPE html>
+                    <html> 
+                        <head>
+                            <meta charset="utf-8" />
+                            <link rel="stylesheet" href="examStyles.css" />
+                        </head>
+                        <body>
+                            ${pageHTML}
+                        </body>
+                    </html>`;
+                fileDownload(template, this.state.filename + this.state.format);
                 break;
             case ".json":
                 fileDownload(JSON.stringify(questions), this.state.filename + this.state.format);
@@ -80,42 +91,54 @@ class ExamPage extends React.Component {
     }
 
     render() {
+        window.scrollTo(0, 0);
         if (this.props.user === false) {
             return (null);
         }
         return (
             <div>
                 <div id="exam">
-                    <div style={{ fontFamily: "Serif", fontSize: "12pt", margin: '.5in' }}>
-                        {!this.props.user ?
-                            <Redirect to={"/login"} />
-                            :
-                            <div>
-                                {this.props.location.state.questions.map((q, key) => {
-                                    let number = this.props.location.state.questions.indexOf(q) + 1
-                                    return (
-                                        <div key={key}>
-                                            <p>{number + ". " + q.question}</p>
-                                            {q.choices.length > 0 ?
-                                                <ul>
-                                                    <p>A. {q.choices[0]}</p>
-                                                    <p>B. {q.choices[1]}</p>
-                                                    <p>C. {q.choices[2]}</p>
-                                                    <p>D. {q.choices[3]}</p>
-                                                </ul>
-                                                :
-                                                <div style={{ height: `${(q.spacing ? q.spacing : 0) + 0.5}in` }}></div>
-                                            }
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        }
-                    </div>
+                    {!this.props.user ?
+                        <Redirect to={"/login"} />
+                        :
+                        <div>
+                            {this.props.location.state.questions.map((q, key) => {
+                                let number = this.props.location.state.questions.indexOf(q) + 1
+                                return (
+                                    <div key={key} id={"question"}>
+                                        <p>{number + ". " + q.question}</p>
+                                        {q.choices.length > 0 ?
+                                            <ul>
+                                                <p>A. {q.choices[0]}</p>
+                                                <p>B. {q.choices[1]}</p>
+                                                <p>C. {q.choices[2]}</p>
+                                                <p>D. {q.choices[3]}</p>
+                                            </ul>
+                                            :
+                                            <div style={{ height: `${(q.spacing ? q.spacing : 0) + 1}in` }}></div>
+                                        }
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    }
                 </div>
                 <Dialog onClose={() => this.setState({ open: false })} open={this.state.open}>
-                    <DialogTitle id="simple-dialog-title">Download Exam</DialogTitle>
+                    <DialogTitle id="simple-dialog-title">Export Exam</DialogTitle>
                     <DialogContent>
+                        If you wish to print the exam with pre-selected formatting such as 1 inch margins
+                        and 17pt font size, click the print button.
+                        If you wish to further customize the exam or
+                        download it in other file formats, please do so below.
+
+                        <br />
+
+                        <Button style={{ margin: '1em' }} color="primary" variant="contained" onClick={() => window.print()}>
+                            Print
+                        </Button>
+
+                        <br />
+
                         <TextField
                             style={{ width: 150 }}
                             label="Filename"
@@ -132,8 +155,8 @@ class ExamPage extends React.Component {
                             select
                         >
                             <MenuItem value=".html">.html</MenuItem>
-                            <MenuItem value=".json">.json</MenuItem>
                             <MenuItem value=".txt">.txt</MenuItem>
+                            <MenuItem value=".json">.json</MenuItem>
                             <MenuItem value=".yaml">.yaml</MenuItem>
                         </TextField>
 
@@ -142,11 +165,9 @@ class ExamPage extends React.Component {
                         <Button style={{ margin: '1em' }} color="primary" variant="contained" onClick={this.downloadFile}>
                             Download
                         </Button>
+
                     </DialogContent>
                 </Dialog>
-                <Fab onClick={() => window.print()} color="primary" aria-label="add" style={{ position: "fixed", bottom: 100, right: 50 }}>
-                    <PrintIcon />
-                </Fab>
                 <Fab onClick={() => this.setState({ open: true })} color="primary" aria-label="add" style={{ position: "fixed", bottom: 30, right: 50 }}>
                     <DownloadIcon />
                 </Fab>
