86118ee0c848584aa82f4df4265ed1d9769d56b8
devyboy <dev@udel.edu>
2019-12-05 11:16:20 -0500

    client side auth

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import { Redirect } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import UDLogo from "../UDMonogram.jpg";
@@ -21,50 +22,67 @@ let styles = {
     fontSize: "1.2em"
   },
   logo: {
-    maxWidth: "200px", 
-    display: "block", 
-    marginLeft: "auto", 
+    maxWidth: "200px",
+    display: "block",
+    marginLeft: "auto",
     marginRight: "auto",
     marginBottom: "2em"
   }
 }
 
 const LoginPage = (props) => {
-
+  let usersArr = [];
+  
   const login = () => {
     let provider = new firebase.auth.GoogleAuthProvider();
-    // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
-    //   firebase.auth().signInWithPopup(provider);
-    // });
-    firebase.auth().signInWithPopup(provider);
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function () {
+      firebase.auth().signInWithPopup(provider)
+        .then((authObj) => {
+          if (!usersArr.includes(authObj.user.email)) {
+            firebase.auth().signOut();
+            alert("You are unauthorized to access this application. Please contact an administrator.");
+          }
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    });
   }
 
-    if (props.user === false) {
-      return (null);
-    }
-    if (props.user === null) {
-      return (
-        <div
-          style={styles.container}
+  firebase.firestore().collection('users').get().then(snapshot => {
+    snapshot.forEach(doc => {
+      usersArr.push(doc.id);
+    });
+  }).catch(err => {
+    console.log(err.message);
+  });
+
+  if (props.user === false) {
+    return (null);
+  }
+  if (props.user === null) {
+    return (
+      <div
+        style={styles.container}
+      >
+        <img src={UDLogo} alt="logo" style={styles.logo} />
+        <h2>To proceed, please login below</h2>
+        <Button
+          style={styles.button}
+          color="primary"
+          variant="contained"
+          onClick={() => login()}
         >
-          <img src={UDLogo} alt="logo" style={styles.logo}/>
-          <h2>To proceed, please login below</h2>
-          <Button
-            style={styles.button}
-            color="primary"
-            variant="contained"
-            onClick={() => login()}
-          >
-            Login
+          Login
           </Button>
-        </div>
-      );
-    }
-    else {
-      return (
-        <Redirect to={{ pathname: "/" }} />
-      );
-    }
+      </div>
+    );
+  }
+  else {
+    return (
+      <Redirect to={{ pathname: "/" }} />
+    );
+  }
 }
 
 export default LoginPage;
