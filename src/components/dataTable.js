86dc3854b0eb0c6733861d4258c9ea34caa5530d
devyboy <dev@udel.edu>
2019-12-15 23:31:43 -0500

    fix table icons, space columns better

--- src/components/dataTable.js
+++ src/components/dataTable.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import MaterialTable from 'material-table';
 import Button from '@material-ui/core/Button';
 import EditIcon from "@material-ui/icons/Edit";
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import FilterList from '@material-ui/icons/FilterList';
+import Search from '@material-ui/icons/Search';
+import Clear from '@material-ui/icons/Clear';
+import Assignment from '@material-ui/icons/Assignment';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+
+
+const tableIcons = {
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  Assignment: forwardRef((props, ref) => <Assignment {...props} ref={ref} />),
+}
 
 class DataTable extends React.Component {
   constructor(props) {
@@ -15,6 +37,7 @@ class DataTable extends React.Component {
     return (
       <MaterialTable
         title={"Question Table"}
+        icons={tableIcons}
         columns={[
           {
             title: '', field: 'edit', filtering: false, sorting: false,
@@ -22,16 +45,17 @@ class DataTable extends React.Component {
           },
           { title: 'Content', field: 'question' },
           {
-            title: 'Question Type', field: 'type',
+            title: 'Type', field: 'type',
             lookup: {
               "Multiple Choice": "Multiple Choice",
               "Free Response": "Free Response",
               "Programming": "Programming"
-            }
+            },
+            cellStyle: {width: "160px"}
           },
-          { title: 'Course', field: 'course' },
+          { title: 'Course', field: 'course', cellStyle: {width: "50px"} },
           { title: 'Topic', field: 'topic' },
-          { title: 'Unit', field: 'unit' },
+          { title: 'Unit', field: 'unit', cellStyle: {width: "150px"} },
           {
             title: 'Difficulty', field: 'diff',
             lookup: {
